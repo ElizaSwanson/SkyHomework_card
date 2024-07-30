d0f4551ca9ed095daad4369adcdb5e7ee1bdb550
Elizaveta Nasytko <Liz99246@gmail.com>
2024-07-30 18:55:34 +0300

    ready

--- src/widget.py
+++ src/widget.py
@@ -5,7 +5,14 @@ def mask_account_card(card_acc_number: str) -> str:
     """это функция для маскировки номеров карт и счетов"""
     if "Счет" in card_acc_number:
         account_number = card_acc_number[-20:]
-        return card_acc_number[:-20] + src.masks.get_mask_account(account_number)
+        return f"{card_acc_number[:-20]} {src.masks.get_mask_account(account_number)}"
     else:
         card_number = "".join(card_acc_number[-16:].split())
-        return card_acc_number[:-16] + src.masks.get_mask_card_number(card_number)
+        return f"{card_acc_number[:-16]} {src.masks.get_mask_card_number(card_number)}"
+
+
+def get_data(data_input: str) -> str | None:
+    """Преобразование даты"""
+    data_ = data_input.split("Т")[0]
+    formated_data = f"{data_[-2:]},{data_[5:7]},{data_[:4]}"
+    return formated_data
