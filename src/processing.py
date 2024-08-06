1bf91a8fbd5369b4c3c2ae809ada5214fe73662b
Elizaveta Nasytko <Liz99246@gmail.com>
2024-08-06 10:45:12 +0300

    Sort by date is done

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,9 @@
 from typing import Any, List, Dict
 
 
-def filter_by_state(state_dict:  list[dict[str, str | Any]], state_status: str = "EXECUTED") -> list[dict[str, str | Any]]:
+def filter_by_state(
+    state_dict: list[dict[str, str | Any]], state_status: str = "EXECUTED"
+) -> list[dict[str, str | Any]]:
     filtered_dict = []
     for data in state_dict:
         if data.get("state") == state_status:
@@ -21,11 +23,23 @@ print(
 )
 
 
-def sort_by_date(state_dict: list[dict[str, any]], reverse: bool = True) -> list[dict[str, any]]:
+def sort_by_date(state_dict: list[dict[str, any]], reversed_list: bool = True) -> list[dict[str, any]]:
     """в этой функции происходит сортировка словарей по дате"""
     sorted_list = sorted(
         state_dict,
-        key=lambda new_list_of_dict: new_list_of_dict["date"],
-        reverse=True,
+        key=lambda new_list: new_list["date"],
+        reverse=reversed_list,
     )
     return sorted_list
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
