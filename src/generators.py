4aebaea48427288c4ca47ec7626f0676c4a0b75f
Elizaveta Nasytko <Liz99246@gmail.com>
2024-08-11 21:20:07 +0300

    kill me

--- src/generators.py
+++ src/generators.py
@@ -1,24 +1,24 @@
 from typing import Any, Generator
 
+import pytest
 
-def filter_by_currency(transactions_list: list, code_of_currency: str = "USD") -> Generator[Any, Any, Any]:
+from test.conftest import transactions
+
+def filter_by_currency(transactions: list, code_of_currency: str = "USD") -> Generator[Any, Any, Any]:
     """Функция возвращает итератор с заданной валютой"""
-    if len(transactions_list) > 0:
-        for transaction in transactions_list:
-            if transaction.get("operationAmount").get("currency").get("code") == code_of_currency:
-                yield transaction
-            elif transaction.get("operationAmount").get("currency").get("code") != code_of_currency:
-                raise AssertionError("Такой код не найден")
-    if transactions_list == ():
-        raise AssertionError("Пустой список")
+    for transaction in transactions:
+        if transaction.get("operationAmount").get("currency").get("code") == code_of_currency:
+            yield transaction
+        elif transactions == ():
+            return "Пустой список"
 
 
 def transaction_descriptions(transactions: list) -> Generator[Any, Any, Any]:
     """сортировка по описанию операции"""
-    for description in transactions:
-        yield description.get("description")
-    if not description:
-        raise ValueError("Такой транзакции нет")
+    for transaction in transactions:
+        description = transaction.get("description")
+        if description:
+            yield description
 
 
 def card_number_generator(start: int, stop: int) -> Generator[str, Any, Any]:
@@ -26,5 +26,6 @@ def card_number_generator(start: int, stop: int) -> Generator[str, Any, Any]:
     for number in range(start, stop + 1):
         starting_number = "0000000000000000"
         number_str = str(number)
-        card_number = (f"{starting_number[: -len(number_str)]}{number_str}")
-        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        card_number = f"{starting_number[: -len(number_str)]}{number_str}"
+        gen_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        yield gen_number
--- test/test_generators.py
+++ test/test_generators.py
@@ -5,26 +5,22 @@ from src.generators import card_number_generator, filter_by_currency, transactio
 
 def test_filter_by_currency_main(transactions):
     generator = filter_by_currency(transactions, "USD")
-    assert next(generator) == transactions[0]
-    assert next(generator) == transactions[1]
+    assert next(generator) == transactions [0]
+    assert next(generator) == transactions [1]
 
 
-def test_filter_by_zero_currency(transactions):
-    with pytest.raises(AssertionError):
-        assert filter_by_currency(transactions) == "Пустой список"
+@pytest.mark.parametrize('transactions_1, code_of_currency_1, expected', [((), "RUB", "Пустой список")])
+def test_filter_by_no_currency(transactions_1, code_of_currency_1, expected):
+    generator = filter_by_currency(transactions_1, code_of_currency_1)
+    assert generator == expected
+
+def test_desc_transact():
+    assert transaction_descriptions(transactions) == ["Перевод организации",
+                                                      "Перевод со счета на счет",
+                                                      "Перевод с карты на карту"
+                                                      "Перевод организации"]
 
 
-def test_filter_another_currency(transactions):
-    with pytest.raises(AssertionError):
-        assert filter_by_currency(transactions, "yen") == "Такой код не найден"
-
-
-def test_desc_transact(transactions):
-    desc_ = transaction_descriptions(transactions)
-    for i in range(4):
-        print(next(desc_))
-
-
-def test_card_n_generator():
-    for card_num in card_number_generator(1, 5):
-        print(next(card_num))
+@pytest.mark.parametrize("start, stop, expected", [(28, 29, "0000 0000 0000 0028")])
+def test_card_n_generator(start, stop, expected):
+    assert next(card_number_generator(start, stop)) == expected
