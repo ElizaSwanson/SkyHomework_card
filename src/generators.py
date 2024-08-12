165ec9d55b097ff023a2c5a54104044a36b85da6
Elizaveta Nasytko <Liz99246@gmail.com>
2024-08-12 20:48:11 +0300

    idk if its done but i tried

--- src/generators.py
+++ src/generators.py
@@ -1,24 +1,26 @@
+from test.conftest import transactions
 from typing import Any, Generator
 
 import pytest
 
-from test.conftest import transactions
 
-def filter_by_currency(transactions: list, code_of_currency: str = "USD") -> Generator[Any, Any, Any]:
+def filter_by_currency(transactions: list[dict], code_of_currency: str = "USD") -> Generator[Any, Any, Any]:
     """Функция возвращает итератор с заданной валютой"""
     for transaction in transactions:
-        if transaction.get("operationAmount").get("currency").get("code") == code_of_currency:
-            yield transaction
-        elif transactions == ():
-            return "Пустой список"
+        try:
+            currency_code = transaction["operationAmount"]["currency"]["code"]
+        except KeyError:
+            continue
+        else:
+            if currency_code == code_of_currency:
+                yield transaction
 
 
-def transaction_descriptions(transactions: list) -> Generator[Any, Any, Any]:
+def transaction_descriptions(transactions: list[dict]) -> Generator[Any, Any, Any]:
     """сортировка по описанию операции"""
     for transaction in transactions:
-        description = transaction.get("description")
-        if description:
-            yield description
+        description = transaction["description"]
+        yield description
 
 
 def card_number_generator(start: int, stop: int) -> Generator[str, Any, Any]:
--- test/test_generators.py
+++ test/test_generators.py
@@ -5,20 +5,14 @@ from src.generators import card_number_generator, filter_by_currency, transactio
 
 def test_filter_by_currency_main(transactions):
     generator = filter_by_currency(transactions, "USD")
-    assert next(generator) == transactions [0]
-    assert next(generator) == transactions [1]
+    assert next(generator) == transactions[0]
+    assert next(generator) == transactions[1]
 
 
-@pytest.mark.parametrize('transactions_1, code_of_currency_1, expected', [((), "RUB", "Пустой список")])
-def test_filter_by_no_currency(transactions_1, code_of_currency_1, expected):
-    generator = filter_by_currency(transactions_1, code_of_currency_1)
-    assert generator == expected
-
-def test_desc_transact():
-    assert transaction_descriptions(transactions) == ["Перевод организации",
-                                                      "Перевод со счета на счет",
-                                                      "Перевод с карты на карту"
-                                                      "Перевод организации"]
+def test_desc_transact(transactions):
+    result = transaction_descriptions(transactions)
+    assert next(result) == "Перевод организации"
+    assert next(result) == "Перевод со счета на счет"
 
 
 @pytest.mark.parametrize("start, stop, expected", [(28, 29, "0000 0000 0000 0028")])
