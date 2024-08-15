b67fc833d5c856d42afcd1b0ade5709b63e5b26a
Elizaveta Nasytko <Liz99246@gmail.com>
2024-08-15 14:16:40 +0300

    decorators _1

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,10 @@
 from functools import wraps
 from typing import Callable, Any
 
+
 def log(filename: str | None = None) -> Callable:
+    """функция выводит описание ошибки в консоль или записывает ее в текстовый файл"""
+
     def log_decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -9,7 +12,7 @@ def log(filename: str | None = None) -> Callable:
                 result = func(*args, **kwargs)
                 if filename:
                     with open(filename, "a", encoding="utf-8") as file:
-                            file.write(f"\n my function is OK")
+                        file.write(f"\n my function is OK")
                 else:
                     print(f"\n my function is OK")
             except Exception as e:
@@ -19,5 +22,7 @@ def log(filename: str | None = None) -> Callable:
                 else:
                     print(f"\n my function error: {e}, inputs: {args}, {kwargs} \n")
             return result
+
         return wrapper
+
     return log_decorator
--- test/test_decorators.py
+++ test/test_decorators.py
@@ -1,9 +1,33 @@
 from src.decorators import log
-import pytest
+
 
 def test_log(capsys):
     @log(filename="mylog.txt")
     def my_func(x, y):
         return x + y
 
-    my_func(1,1)
+    my_func(1, 1)
+
+
+def test_log_not_OK(capsys):
+    @log(filename="mylog.txt")
+    def my_func_1(x, y):
+        return x + y
+
+    my_func_1(9)
+
+
+def test_log_NO_filename(capsys):
+    @log()
+    def my_func(x, y):
+        return x + y
+
+    my_func(1, 3)
+
+
+def test_log_filename_NOT_OK(capsys):
+    @log()
+    def my_func(x, y):
+        return x + y
+
+    my_func(1)
